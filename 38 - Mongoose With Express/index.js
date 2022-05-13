73116120cdf42d2589bfbbb850415883ccd62914
Trey Coggins <coggins.mp3@gmail.com>
2022-05-13 18:03:32 -0400

    add lecture 38 and rename 8

--- 38 - Mongoose With Express/index.js
+++ 38 - Mongoose With Express/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const res = require('express/lib/response');
+const { default: mongoose } = require('mongoose');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27107/farmStand', {useNewParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('MONGO CONNECTION OPEN!');
+  })
+  .catch(err => {
+    console.log('OH NO, MONGO CONNECTION ERROR!')
+  });
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
