6ecd98664c3f7b0e8a1e71dc92ca4b93bd2cc6c8
Trey Coggins <coggins.mp3@gmail.com>
2022-05-13 22:37:05 -0400

    update 38

--- 38 - Mongoose With Express/index.js
+++ 38 - Mongoose With Express/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
@@ -17,14 +18,23 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {
     console.log(err);
   });
 
+
+  
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({})
-  console.log(products);
-  res.send('ALL PRODUCTS WILL BE HERE!');
+  res.render('products/index', { products });
 });
+
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  console.log(product);
+  res.render('products/show', { product });
+});
+
 
 app.listen(PORT, () => {
   console.log(`LISTENING ON PORT ${PORT}`);
