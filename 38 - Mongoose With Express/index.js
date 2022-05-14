ea3e6230540f1b0d8f60a35370452ce1503f175b
Trey Coggins <coggins.mp3@gmail.com>
2022-05-14 14:28:19 -0400

    add 38

--- 38 - Mongoose With Express/index.js
+++ 38 - Mongoose With Express/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const res = require('express/lib/response');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const methodOverride = require('method-override');
+
 const Product = require('./models/product');
+const res = require('express/lib/response');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', {
     useNewUrlParser: true,
@@ -22,16 +24,25 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {
   
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => {
-  const products = await Product.find({})
-  res.render('products/index', { products });
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render('products/index', { products, category });
+
+  } else {}
+  const products = await Product.find({});
+  res.render('products/index', { products, category: 'All' });
 });
 
 app.get('/products/new', (req, res) => {
-  res.render('products/new');
+  res.render('products/new', { categories });
 });
 
 app.post('/products', async (req, res) => {
@@ -45,6 +56,24 @@ app.get('/products/:id', async (req, res) => {
   const product = await Product.findById(id);
   console.log(product);
   res.render('products/show', { product });
+});
+
+app.get('/products/:id/edit', async (req, res) =>{
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', { product, categories });
+});
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true});
+  res.redirect(`/products/${product._id}`);
+});
+
+app.delete('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  res.redirect('/products');
 });
 
 
