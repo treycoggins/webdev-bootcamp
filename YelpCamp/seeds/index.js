93e279e4dff73f61470bcafaf61ef0cdc097fdf0
Trey Coggins <coggins.mp3@gmail.com>
2022-05-15 00:39:03 -0400

    add yelp-camp

---
+++ YelpCamp/seeds/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const res = require('express/lib/response');
+const mongoose = require('mongoose');
+const path = require('path');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      title: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
